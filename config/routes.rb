a7bcf08fe59382a76995b7d387ced03da6264a86
岸波　雄樹 <you_work_24@icloud.com>
2019-10-23 19:37:20 +0800

    add devise and add root/pages#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+  devise_scope :user do
+    get "user/:id", to: "users/registrations#detail"
+    get "signup", to: "users/registrations#new"
+    get "login", to: "users/sessions#new"
+    get "logout", to: "users/sessions#destroy"
+  end
 
-  
-  root to: 'rooms#show'
+  root to: "pages#index"
+
+  # チャット用
+  get "room/show", to: "rooms#show"
+  # root to: 'rooms#show'
   mount ActionCable.server => './cable'
 end
