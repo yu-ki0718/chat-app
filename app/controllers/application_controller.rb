808c5e2cdb1ee2b0cc019640b52f7011ffb77c53
岸波　雄樹 <you_work_24@icloud.com>
2019-11-02 15:46:33 +0800

    modified css and  ja.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
     # ログイン後のリダイレクト先
     def after_sign_in_path_for(resource)
-        "room/show"
+        "/room/show"
     end
 
     protected
